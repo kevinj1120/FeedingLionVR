2c7c2164ba3890de5a74bb4ae8cbbe0f5a0f603e
kevinj1120 <kevin.jung02@gmail.com>
2021-05-05 17:39:50 -0700

    Feeding the Lions

---
+++ EscapeRoomTemplate Real Test/Assets/Mouth.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouth : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other){
+        //detect meat gameObject
+        //once meat touches lion it disappears
+        //make lion disappear
+        if (other.gameObject.CompareTag("Meat")){
+            Destroy(other.gameObject);
+        }
+    }
+}
