3a509a4e65b6fd9434533d3e3624564b0b2c4016
kevinj1120 <kevin.jung02@gmail.com>
2021-06-18 12:22:00 -0700

    Added customGrabber

---
+++ EscapeRoomTemplate Real Test/Assets/BehaviourManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviourManager : MonoBehaviour
+{
+    public OVRGrabber leftHand;
+    public OVRGrabber rightHand;
+    public GameObject grabbedObject;
+
+    List<FollowPlayer> tigers = new List<FollowPlayer>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+    public void grabTest() //test
+    {
+        Debug.Log("Grabbing: " + grabbedObject);
+    }
+    private bool CheckForMeatGrab()
+    {
+        //this updates and looks for grabbing of the meat
+        //returns true if grabbing meat.
+        return false;
+    }
+
+    private void SetTigersToFollow()
+    {
+        //go through each tiger in list.
+        //check if distance is close enough
+        //if yes, set to follow.
+    }
+}
---
+++ EscapeRoomTemplate Real Test/Assets/FollowPlayer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent agent;
+    [SerializeField] private Transform player;
+    [SerializeField] private float distanceFromPlayer;
+    [SerializeField] private float distanceThreshold;
+    [SerializeField] private bool isFollowing = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GetDistanceToTarget(player) < distanceThreshold && isFollowing)
+        {
+            agent.destination = player.transform.position;
+        }
+        
+        //Debug.Log("Distance: " + GetDistance());
+    }
+
+    private float GetDistanceToTarget(Transform target)
+    {
+        distanceFromPlayer = Vector3.Distance(transform.position, target.position);
+        return distanceFromPlayer;
+    }
+}
